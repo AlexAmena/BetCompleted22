4ca00d0b6f8edec7032f2c5df212ca549888ca4c
AlexAmena <aamenabar012@ikasle.ehu.eus>
2022-09-21 21:29:44 +0200

    Alejon bugek yasta

--- src/main/java/domain/Apustua.java
+++ src/main/java/domain/Apustua.java
@@ -74,7 +74,8 @@ public class Apustua implements Serializable{
 	}
 	
 	public boolean galdutaMarkatu(Quote quo) {
-		if(kuota.getQuestion().getQuestionNumber()==quo.getQuestion().getQuestionNumber() && quo.getQuoteNumber()!=kuota.getQuoteNumber()) {
+		if(kuota.getQuestion().getQuestionNumber().equals(quo.getQuestion().getQuestionNumber()) && 
+				!quo.getQuoteNumber().equals(kuota.getQuoteNumber())) {
 			this.egoera="galduta";
 			return true;
 		}
@@ -87,10 +88,10 @@ public class Apustua implements Serializable{
 	
 	@Override
 	public boolean equals(Object o) {
+		if(o==null) return false;
+		if(this.getClass() != o.getClass()) return false;
 		Apustua a = (Apustua) o; 
-		if(a==null) {
-			return false;
-		}
+		
 		return this.getApostuaNumber().equals(a.getApostuaNumber()); 
 	}
 }
